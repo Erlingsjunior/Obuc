7f646df9cccf9801f90fe0a622b5c39c87b391ec
ErlingSJunior <erlingjunior@gmail.com>
2021-01-07 20:32:16 -0300

    Primeiro Commit Iniciando Projeto

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Starting Project
         </a>
       </header>
     </div>
